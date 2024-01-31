003142ec053e9bfb0d48a6801fef3af085643f5c
quintinpeterson1 <157077856+quintinpeterson1@users.noreply.github.com>
2024-01-31 01:05:38 +0000

    homework 1

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -1,12 +1,18 @@
 #include "data_types.h"
 
-//example
-int add_numbers(int num1, int num2)
+int multiply_numbers(int num1)
 {
-	return num1 + num2;
+	//write function code here
+	int num2 = 5;
+
+	int return_value = num1 * num2;
+
+	return return_value;
+
+	
 }
 
-//write function code here
+
 
 
 
