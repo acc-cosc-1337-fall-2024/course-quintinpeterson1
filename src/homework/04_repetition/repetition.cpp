b6a9863a74492ae8906dcb3f138f309dd10725a8
quintinpeterson1 <157077856+quintinpeterson1@users.noreply.github.com>
2024-02-23 18:16:36 +0000

    homework 4

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,39 @@
 //add include statements
+#include "repetition.h"
+
 
 //add function(s) code here
+int factorial(int num)
+{
+    int sum = 1;
+    
+    while (num > 0) {
+        sum *= num;
+        num--;
+    }
+
+
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+    for(; num1 != num2;) {
+
+        if(num1 < num2) {
+            int swap = num1;
+            num1 = num2;
+            num2 = swap;
+        }
+
+        if(num1 > num2) {
+            num1 -= num2;
+        }
+
+        
+
+    }
+
+
+    return num1;
+}
